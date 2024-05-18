1348e97d209a459a50051afc15caf6da5ba112a5
George-Mikulich <g.mikulich@softteco.com>
2024-05-18 15:05:21 +0300

    add tf config

---
+++ main.tf
@@ -0,0 +1,122 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_name
+  region  = var.region
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-custom-mode-network"
+  auto_create_subnetworks = false
+  mtu                     = 1460
+}
+
+resource "google_compute_subnetwork" "default" {
+  name          = "my-custom-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_instance" "default2" {
+  name         = "flask-vm"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install Flask
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq build-essential python3-pip rsync; pip install flask"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+  }
+}
+
+resource "google_compute_instance" "default" {
+  name         = "nginx-vm"
+  machine_type = "f1-micro"
+  zone         = var.zone
+  tags         = ["ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  # Install nginx
+  metadata_startup_script = "sudo apt-get update; sudo apt-get install --assume-yes nginx"
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.default.id
+
+    access_config {
+      # Include this section to give the VM an external IP address
+    }
+  }
+}
+
+resource "google_compute_firewall" "ssh" {
+  name = "allow-ssh"
+  allow {
+    ports    = ["22"]
+    protocol = "tcp"
+  }
+  direction     = "INGRESS"
+  network       = google_compute_network.vpc_network.id
+  priority      = 1000
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["ssh"]
+}
+
+resource "google_compute_firewall" "flask" {
+  name    = "flask-app-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "nginx" {
+  name    = "nginx-server-firewall"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instance"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection = "true"
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  description = "Name of gcp project"
+  type        = string
+  default     = "primal-catfish-423408-p7"
+}
+variable "region" {
+  description = "region"
+  type        = string
+  default     = "europe-central2"
+}
+variable "zone" {
+  description = "zone"
+  type        = string
+  default     = "europe-central2-b"
+}
